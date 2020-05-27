9a97233880217c5481bdc96ac0f353b9bc21805a
richardmoyer <richmoyer@gmail.com>
2020-05-27 10:35:21 -0500

    add comment

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn get_version() -> u16 {
     1000 // could use: return 1000; , only use return for an early return
 }
-
+// comment
 fn usage() {
     let the_version: u16;
     the_version = get_version();
