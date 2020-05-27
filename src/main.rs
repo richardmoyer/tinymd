c8f9ac04c0c921af646d8811821154ebd09d9af7
richm <richmoyer@gmail.com>
2020-05-27 08:19:32 -0500

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn get_version() -> u16 {
+    1000 // could use: return 1000; , only use return for an early return
+}
+
+fn usage() {
+    let the_version: u16;
+    the_version = get_version();
+
+    println!("tinymd, a markdown compiler written by richm.");
+    println!("Version {}", the_version);
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
